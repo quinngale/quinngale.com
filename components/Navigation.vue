992204246a6d1cad186e2ab4c3842c4568caa6d0
Quinn Gale <q.gale@live.com>
2022-12-31 13:32:29 -0700

    Added nav bar content with basic toggle functionality

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,3 +1,35 @@
 <template>
-    nav
+    <nav id="site-nav" class="nav" :class="{ collapsed: isNavCollapsed }">
+        <ul>
+            <li class="nav-brand">
+                <Logo />
+            </li>
+            <li class="nav-link">
+                <NuxtLink>Home</NuxtLink>
+            </li>
+            <li class="nav-link">
+                <NuxtLink>About</NuxtLink>
+            </li>
+            <li class="nav-link">
+                <NuxtLink>Portfolio</NuxtLink>
+            </li>
+            <li class="nav-link">
+                <NuxtLink>Contact</NuxtLink>
+            </li>
+        </ul>
+        <button @click="isNavCollapsed = !isNavCollapsed">
+            <font-awesome-icon icon='fa-regular fa-bars' />
+            <span>Menu</span>
+        </button>
+    </nav>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            isNavCollapsed: false
+        }
+    }
+}
+</script>
