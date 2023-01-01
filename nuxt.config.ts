dde7bd4e0b24f3cf4671057e6b08d0779733ea1c
Quinn Gale <q.gale@live.com>
2023-01-01 16:59:06 -0700

    FA was duplicating text and not finding an icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	css: ['~/assets/css/main.css', '@fortawesome/fontawesome-svg-core/styles.css'],
+	css: ['@fortawesome/fontawesome-svg-core/styles.css', '~/assets/css/main.css'],
+
+	build: {
+		transpile: ['@fortawesome/vue-fontawesome', '@fortawesome/fontawesome-svg-core', '@fortawesome/pro-regular-svg-icons'],
+	},
 });
