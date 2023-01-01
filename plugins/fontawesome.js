5feda42b2df9e71039e539dcff3e2b576e0a247e
Quinn Gale <q.gale@live.com>
2023-01-01 16:59:28 -0700

    Limited icon imports

--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -1,10 +1,11 @@
 import { library, config } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { far } from '@fortawesome/pro-regular-svg-icons'
+import { faBars } from '@fortawesome/pro-regular-svg-icons'
 
-config.autoAddCss = false
-library.add(far)
+config.autoAddCss = false;
+
+library.add(faBars);
 
 export default defineNuxtPlugin((nuxtApp) => {
-    nuxtApp.vueApp.component('font-awesome-icon', FontAwesomeIcon, {})
+    nuxtApp.vueApp.component('font-awesome-icon', FontAwesomeIcon, {});
 })
