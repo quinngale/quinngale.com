5a37de210587b8788c067967f3108806839b9e90
Quinn Gale <q.gale@live.com>
2023-01-01 22:47:24 -0700

    @nuxt/content is now enabled and functioning

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	css: ['@fortawesome/fontawesome-svg-core/styles.css', '~/assets/fonts/Inter/inter.css', '~/assets/css/main.css'],
-
 	build: {
 		transpile: ['@fortawesome/vue-fontawesome', '@fortawesome/fontawesome-svg-core', '@fortawesome/pro-regular-svg-icons'],
 	},
+
+	css: ['@fortawesome/fontawesome-svg-core/styles.css', '~/assets/fonts/Inter/inter.css', '~/assets/css/main.css'],
+
+	modules: ['@nuxt/content'],
 
 	app: {
 		head: {
