10fdbb1eefffb7946a182d97caab85106368a938
Quinn Gale <q.gale@live.com>
2023-01-01 22:21:02 -0700

    The icons next to the links return

--- components/Footer.vue
+++ components/Footer.vue
@@ -4,11 +4,17 @@
             <ul>
                 <li>
                     <NuxtLink href="mailto:q.gale@live.com">
+                        <span class="icon">
+                            <font-awesome-icon :icon="['regular', 'at']" />
+                        </span>
                         <span>q.gale@live.com</span>
                     </NuxtLink>
                 </li>
                 <li>
                     <NuxtLink href="https://github.com/gabeauim/quinngale.com">
+                        <span class="icon">
+                            <font-awesome-icon :icon="['brands', 'github']" />
+                        </span>
                         <span>GitHub</span>
                     </NuxtLink>
                 </li>
