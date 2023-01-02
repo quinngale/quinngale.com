938c9be0667766b0b2769ce05c36613e9edcd080
Quinn Gale <q.gale@live.com>
2023-01-01 18:20:07 -0700

    Added Inter as the site font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	css: ['@fortawesome/fontawesome-svg-core/styles.css', '~/assets/css/main.css'],
+	css: ['@fortawesome/fontawesome-svg-core/styles.css', '~/assets/fonts/Inter/inter.css', '~/assets/css/main.css'],
 
 	build: {
 		transpile: ['@fortawesome/vue-fontawesome', '@fortawesome/fontawesome-svg-core', '@fortawesome/pro-regular-svg-icons'],
