f992fabd4e118b6355f8218683ce6d3afbd31288
Quinn Gale <q.gale@live.com>
2023-01-01 21:38:43 -0700

    Added two more FA icons: GitHub and evelope

--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -1,10 +1,12 @@
 import { library, config } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faAngleUp, faAngleDown } from '@fortawesome/pro-regular-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/pro-solid-svg-icons';
 
 config.autoAddCss = false;
 
-library.add(faAngleUp, faAngleDown);
+library.add(faAngleUp, faAngleDown, faGithub, faEnvelope);
 
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.component('font-awesome-icon', FontAwesomeIcon, {});
