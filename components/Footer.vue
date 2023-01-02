a0da6e1893fb99721c323a7cb80c1ca6f0eab462
Quinn Gale <q.gale@live.com>
2023-01-01 22:12:52 -0700

    Added real footer contents

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,3 +1,22 @@
 <template>
-    Footer
+    <footer>
+        <section class="container">
+            <ul>
+                <li>
+                    <NuxtLink href="mailto:q.gale@live.com">
+                        <span>q.gale@live.com</span>
+                    </NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink href="https://github.com/gabeauim/quinngale.com">
+                        <span>GitHub</span>
+                    </NuxtLink>
+                </li>
+            </ul>
+
+            <br>
+
+            <p>© 2023 Quinn Gale</p>
+        </section>
+    </footer>
 </template>
