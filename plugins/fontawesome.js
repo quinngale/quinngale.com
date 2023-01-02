57993a78b498a2e23050cc22f423cd375afe378d
Quinn Gale <q.gale@live.com>
2023-01-01 22:21:36 -0700

    Replace 'envelope' with 'at'

--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -1,12 +1,11 @@
 import { library, config } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faAngleUp, faAngleDown } from '@fortawesome/pro-regular-svg-icons'
+import { faAngleUp, faAngleDown, faAt } from '@fortawesome/pro-regular-svg-icons'
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope } from '@fortawesome/pro-solid-svg-icons';
 
 config.autoAddCss = false;
 
-library.add(faAngleUp, faAngleDown, faGithub, faEnvelope);
+library.add(faAngleUp, faAngleDown, faGithub, faAt);
 
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.component('font-awesome-icon', FontAwesomeIcon, {});
