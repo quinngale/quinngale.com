97d5421c66a5543ccae0e6fb4645b5c9de9262a7
Quinn Gale <q.gale@live.com>
2023-01-22 12:23:47 -0700

    Started work on the home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
     <main>
+        <img src="/profile.jpg" id="profile-image" />
         <h1>
             Hello,
             <br>
@@ -7,3 +8,65 @@
         </h1>
     </main>
 </template>
+
+<style scoped>
+main {
+    display: grid;
+    grid-template-columns: 1;
+    place-content: center;
+    place-items: center;
+    padding: 8rem 0;
+}
+
+#profile-image {
+    border-radius: 100%;
+    width: 60vw;
+    max-width: 320px;
+    animation: image-in 1.2s ease 1000ms backwards;
+}
+
+h1 {
+    animation: text-in 1.2s ease 500ms backwards;
+}
+
+@keyframes text-in {
+    from {
+        opacity: 0;
+        transform: translateY(10vh);
+    }
+
+    to {
+        opacity: 1;
+        transform: translateY(0);
+    }
+}
+
+@keyframes image-in {
+    from {
+        opacity: 0;
+        transform: translateY(-10vh);
+    }
+
+    to {
+        opacity: 1;
+        transform: translateY(0);
+    }
+}
+</style>
+
+<script>
+export default {
+    data() {
+        return {
+            ready: false
+        }
+    },
+
+    methods: {
+        setReady() {
+            console.log('image loaded');
+            this.ready = true;
+        }
+    }
+}
+</script>
