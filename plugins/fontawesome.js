b893f119a657849b1ce8638d3d31181c131b7429
Quinn Gale <q.gale@live.com>
2023-05-07 14:37:20 -0600

    Email address now uses the more standard envelope icon

--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -1,11 +1,12 @@
 import { library, config } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faAngleUp, faAngleDown, faAt } from '@fortawesome/pro-regular-svg-icons'
+import { faAngleUp, faAngleDown } from '@fortawesome/pro-regular-svg-icons'
+import { faEnvelope } from '@fortawesome/pro-solid-svg-icons'
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 config.autoAddCss = false;
 
-library.add(faAngleUp, faAngleDown, faGithub, faAt);
+library.add(faAngleUp, faAngleDown, faGithub, faEnvelope);
 
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.component('font-awesome-icon', FontAwesomeIcon, {});
