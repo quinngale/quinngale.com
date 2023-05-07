e4270d52ab3762ae7de0dc3b74b55fdaf20ed280
Quinn Gale <q.gale@live.com>
2023-05-07 16:14:03 -0600

    Corrected icon imports from the merge

--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -1,12 +1,12 @@
 import { library, config } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faAngleUp, faAngleDown, faAt, faArrowUpRightFromSquare } from '@fortawesome/pro-regular-svg-icons'
-import { faBriefcase, faGraduationCap } from '@fortawesome/pro-solid-svg-icons';
+import { faAngleDown, faArrowUpRightFromSquare } from '@fortawesome/pro-regular-svg-icons'
+import { faBriefcase, faGraduationCap, faEnvelope } from '@fortawesome/pro-solid-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 config.autoAddCss = false;
 
-library.add(faAngleUp, faAngleDown, faGithub, faAt, faBriefcase, faGraduationCap, faArrowUpRightFromSquare);
+library.add(faAngleDown, faEnvelope, faGithub, faBriefcase, faGraduationCap, faArrowUpRightFromSquare);
 
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.component('font-awesome-icon', FontAwesomeIcon, {});
