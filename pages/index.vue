2a27224e77f732ba6f6b841f38350ea34cc1d810
Quinn Gale <q.gale@live.com>
2023-06-17 22:39:27 -0600

    Better headings for content

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,26 @@
 <template>
-    <main>
-        <img src="/profile.jpg" id="profile-image" />
-        <h1>
-            Hello,
-            <br>
-            My name is Quinn Gale
-        </h1>
+    <main class="main">
+        <article class="container">
+            <section class="section">
+                <div>
+                    <img src="/profile.jpg" id="profile-image" />
+                    <h1>
+                        Hello,
+                        <br>
+                        My name is Quinn Gale
+                    </h1>
+                </div>
+            </section>
+            <section class="section">
+                <h2>Who I am</h2>
+
+            </section>
+            <section class="section">
+                <h2>Projects</h2>
+            </section>
+            <section class="section">
+                <h2>How to contact me</h2>
+            </section>
+        </article>
     </main>
 </template>
-
-<style scoped>
-main {
-    display: grid;
-    grid-template-columns: 1;
-    place-content: center;
-    place-items: center;
-    padding: 8rem 0;
-}
-
-#profile-image {
-    border-radius: 100%;
-    width: 60vw;
-    max-width: 320px;
-    animation: image-in 1.2s ease 1000ms backwards;
-}
-
-h1 {
-    animation: text-in 1.2s ease 500ms backwards;
-}
-
-@keyframes text-in {
-    from {
-        opacity: 0;
-        transform: translateY(10vh);
-    }
-
-    to {
-        opacity: 1;
-        transform: translateY(0);
-    }
-}
-
-@keyframes image-in {
-    from {
-        opacity: 0;
-        transform: translateY(-10vh);
-    }
-
-    to {
-        opacity: 1;
-        transform: translateY(0);
-    }
-}
-</style>
-
-<script>
-export default {
-    data() {
-        return {
-            ready: false
-        }
-    },
-
-    methods: {
-        setReady() {
-            console.log('image loaded');
-            this.ready = true;
-        }
-    }
-}
-</script>
