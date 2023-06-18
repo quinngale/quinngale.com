4415871c832f3a4612d04be91180f29b422a3394
Quinn Gale <q.gale@live.com>
2023-06-17 22:37:45 -0600

    Footer links are no longer in a list They will need to get grouped better at some point

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,26 +1,22 @@
 <template>
-    <footer>
+    <footer class="footer">
         <section class="container">
-            <ul>
-                <li>
-                    <NuxtLink href="mailto:quinn@quinngale.com">
-                        <span class="icon">
-                            <font-awesome-icon :icon="['solid', 'envelope']" />
-                        </span>
-                        <span>quinn@quinngale.com</span>
-                    </NuxtLink>
-                </li>
-                <li>
-                    <NuxtLink href="https://github.com/gabeauim/quinngale.com">
-                        <span class="icon">
-                            <font-awesome-icon :icon="['brands', 'github']" />
-                        </span>
-                        <span>GitHub</span>
-                    </NuxtLink>
-                </li>
-            </ul>
-
-            <br>
+            <p>
+                <NuxtLink href="mailto:quinn@quinngale.com" class="link">
+                    <span class="link__icon">
+                        <font-awesome-icon :icon="['solid', 'envelope']" />
+                    </span>
+                    <span>quinn@quinngale.com</span>
+                </NuxtLink>
+            </p>
+            <p>
+                <NuxtLink href="https://github.com/gabeauim/quinngale.com" class="link">
+                    <span class="link__icon">
+                        <font-awesome-icon :icon="['brands', 'github']" />
+                    </span>
+                    <span>GitHub</span>
+                </NuxtLink>
+            </p>
 
             <p>© 2023 Quinn Gale</p>
         </section>
