b7dc7684fd204b64f25f7682da4db99c98fdacb7
Quinn Gale <q.gale@live.com>
2023-06-17 20:24:43 -0600

    Conversion to BEM class naming

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,19 +1,19 @@
 <template>
-    <nav id="site-nav" class="nav" :class="{ expanded: isNavExpanded }">
-        <ul>
-            <li class="nav-brand">
+    <nav id="site-nav" class="nav" :class="{ 'nav__expanded': isNavExpanded }">
+        <ul class="nav__list">
+            <li class="nav__brand">
                 <Logo @click="isNavExpanded = false" />
             </li>
-            <li class="nav-link">
+            <li class="nav__item">
                 <NuxtLink href="/" @click="isNavExpanded = false">Home</NuxtLink>
             </li>
-            <li class="nav-link">
+            <li class="nav__item">
                 <NuxtLink href="/about" @click="isNavExpanded = false">About</NuxtLink>
             </li>
-            <li class="nav-link">
+            <li class="nav__item">
                 <NuxtLink href="/portfolio" @click="isNavExpanded = false">Portfolio</NuxtLink>
             </li>
-            <li class="nav-link">
+            <li class="nav__item">
                 <NuxtLink href="/contact" @click="isNavExpanded = false">Contact</NuxtLink>
             </li>
         </ul>
