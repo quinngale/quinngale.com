7711d4f2826112dc7e743053239b840d90cd07d3
Quinn Gale <q.gale@live.com>
2023-06-17 22:39:14 -0600

    About me page The badge serves as a standard way of identifiying the school/employer.

---
+++ components/badges/WSU.vue
@@ -0,0 +1,3 @@
+<template>
+    <a href="http://weber.edu">Weber State University</a>
+</template>
--- pages/about.vue
+++ pages/about.vue
@@ -1,8 +1,124 @@
 <template>
-    <main>
+    <main class="main">
         <article class="container">
-            <h1>About</h1>
+            <header class="header">
+                <h1 class="header__title">About</h1>
+            </header>
 
+            <section>
+                <h2>Degrees and certificates</h2>
+            </section>
+
+            <section>
+                <h2>Work history</h2>
+                <ul class="timeline">
+                    <li class="timeline__item">
+                        <div class="icon">
+                            <font-awesome-icon :icon="['fa-solid', 'fa-briefcase']" />
+                        </div>
+                        <div class="content">
+                            <p>New job - July 2016</p>
+                            <p>Technical Support Specialist</p>
+                            <div>
+                                <BadgesWSU />
+                            </div>
+                        </div>
+                    </li>
+
+                    <li>
+                        <div class="icon">
+                            <font-awesome-icon :icon="['fa-solid', 'fa-graduation-cap']" />
+                        </div>
+                        <div class="content">
+                            <p>Start college - August 2016</p>
+                            <p><strong>Major: </strong>Computer Science</p>
+                            <div>
+                                <BadgesWSU />
+                            </div>
+                        </div>
+                    </li>
+
+                    <li>
+                        <div class="icon">
+                            <font-awesome-icon :icon="['fa-solid', 'fa-briefcase']" />
+                        </div>
+                        <div class="content">
+                            <p>Position change - March 2019</p>
+                            <p>Technical Support Specialist - Tech Lead</p>
+                            <div>
+                                <BadgesWSU />
+                            </div>
+                        </div>
+                    </li>
+
+                    <li>
+                        <div class="icon">
+                            <font-awesome-icon :icon="['fa-solid', 'fa-graduation-cap']" />
+                        </div>
+                        <div class="content">
+                            <p>Degree change - January 2020</p>
+                            <p><strong>Major: </strong>Web and User Experience</p>
+                            <p><strong>Minor: </strong>Computer Science</p>
+                            <div>
+                                <BadgesWSU />
+                            </div>
+                        </div>
+                    </li>
+
+                    <li>
+                        <div class="icon">
+                            <font-awesome-icon :icon="['fa-solid', 'fa-briefcase']" />
+                        </div>
+                        <div class="content">
+                            <p>Position change - August 2022</p>
+                            <p>Technical Support Specialist - Move to full time position</p>
+                            <div>
+                                <BadgesWSU />
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="icon">
+                            <font-awesome-icon :icon="['fa-solid', 'fa-graduation-cap']" />
+                        </div>
+                        <div class="content">
+                            <p>Graduation - April 2023</p>
+                            <p>Bachelors of Science in Web and User Experience</p>
+                            <div>
+                                <BadgesWSU />
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </section>
         </article>
     </main>
 </template>
+
+<style scoped>
+.timeline {
+    list-style: none;
+    display: flex;
+    flex-flow: column;
+    gap: 2rem;
+}
+
+.timeline li {
+    display: grid;
+    grid-template-columns: auto 1fr;
+    grid-template-rows: 3;
+    grid-template-areas: 'icon content';
+    gap: 0.5rem;
+}
+
+.timeline .icon {
+    background-color: var(--gold);
+    width: 2.5rem;
+    height: 2.5rem;
+    font-size: 1.25rem;
+    display: grid;
+    place-items: center;
+    border-radius: 100%;
+    color: var(--background);
+}
+</style>
