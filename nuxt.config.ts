d77ccab657f2b1a65409c7d2fa3940bade5b8fab
Quinn Gale <q.gale@live.com>
2023-08-09 18:21:20 -0600

    Explicitly defined `ssr` to `true`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	ssr: true,
+
 	build: {
 		transpile: ['@fortawesome/vue-fontawesome', '@fortawesome/fontawesome-svg-core', '@fortawesome/pro-regular-svg-icons'],
 	},
