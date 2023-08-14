a8fc32c5964fd2f7e64378123b34ef063268d1f4
Quinn Gale <q.gale@live.com>
2023-08-13 22:01:21 -0600

    Reorganized icon imports

--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -1,12 +1,19 @@
 import { library, config } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import { faBriefcase, faDatabase, faEnvelope, faFileCertificate, faGraduationCap, faHeadset } from '@fortawesome/pro-solid-svg-icons';
 import { faAngleDown, faArrowUpRightFromSquare } from '@fortawesome/pro-regular-svg-icons'
-import { faBriefcase, faGraduationCap, faEnvelope, faHeadset, faDatabase, faFileCertificate } from '@fortawesome/pro-solid-svg-icons';
-import { faGithub, faJs, faHtml5, faCss3, faVuejs, faPython, faLinux, faMicrosoft } from '@fortawesome/free-brands-svg-icons';
+import { faCss3, faGithub, faHtml5, faLinux, faJs, faMicrosoft, faPython, faVuejs } from '@fortawesome/free-brands-svg-icons';
 
 config.autoAddCss = false;
+// Solid icons
+library.add(faBriefcase, faDatabase, faEnvelope, faFileCertificate, faGraduationCap, faHeadset);
 
-library.add(faAngleDown, faEnvelope, faGithub, faBriefcase, faGraduationCap, faArrowUpRightFromSquare, faHeadset, faJs, faHtml5, faCss3, faVuejs, faPython, faDatabase, faLinux, faMicrosoft, faFileCertificate);
+// Regular icons
+library.add(faAngleDown, faArrowUpRightFromSquare);
+
+// Brand icons
+library.add(faCss3, faGithub, faHtml5, faLinux, faJs, faMicrosoft, faPython, faVuejs);
 
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.component('font-awesome-icon', FontAwesomeIcon, {});
