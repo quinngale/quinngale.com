2a9568efcc0398944f627cecea20c158792ee693
Quinn Gale <q.gale@live.com>
2023-09-16 17:14:12 -0600

    Started working on the home page layout

--- pages/index.vue
+++ pages/index.vue
@@ -2,18 +2,24 @@
     <main>
         <article class="container">
             <section>
-                <div>
-                    <img src="/profile.jpg" id="profile-image" />
-                    <h1>
-                        Hello,
-                        <br>
-                        My name is Quinn Gale
-                    </h1>
+                <div class="columns">
+                    <div class="column">
+                        <img src="/profile.jpg" id="profile-image" />
+                    </div>
+                    <div class="column--span-2 column--medium--span-1" id="header-text">
+                        <h1>
+                            Hello
+                            <br>
+                            My name is Quinn.
+                        </h1>
+                    </div>
                 </div>
             </section>
             <section>
                 <h2>Who I am</h2>
-
+                <p>
+                    
+                </p>
             </section>
             <section>
                 <h2>Projects</h2>
@@ -24,3 +30,16 @@
         </article>
     </main>
 </template>
+
+<style lang="scss" scoped>
+#profile-image {
+    border-radius: 50%;
+}
+
+#header-text {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    align-content: center;
+}
+</style>
