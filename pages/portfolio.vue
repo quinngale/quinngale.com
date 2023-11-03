f450869964e33bb09dcda280fd4275e819ed8271
Quinn Gale <q.gale@live.com>
2023-11-02 21:31:13 -0600

    Removed (currently) unused content

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -8,22 +8,22 @@
 
             <section>
                 <p>
-                    The portfolio is a collection of projects that I have worked on. Most of these are personal projects
+                    The portfolio is a collection of projects and short scripts that I have worked on. Most of these are personal projects
                     that contribute something of dubious usefulness to my life.
                 </p>
             </section>
 
-            <section>
-                <h2>Projects</h2>
-                <ul>
-                    <ContentQuery path="/project" v-slot="{ data }" find>
-                        <li v-for="stub in data" :key="stub._path">
-                            <img :src="stub.image" alt="Placeholder image" />
-                            <ContentRenderer :value="stub" excerpt :components="components" tag="article" />
-                        </li>
-                    </ContentQuery>
-                </ul>
-            </section>
+            <!-- <ContentList path="/project" v-slot="{ list }" find>
+                <section v-if="list">
+                        <h2>Projects</h2>
+                        <ul>
+                            <li v-for="stub in list" :key="stub._path">
+                                <img :src="stub.image" alt="Placeholder image" />
+                                <ContentRenderer :value="stub" excerpt :components="components" tag="article" />
+                            </li>
+                        </ul>
+                </section>
+            </ContentList> -->
 
             <section>
                 <h2>One-offs</h2>
@@ -59,39 +59,3 @@ const components = {
     h1: 'strong'
 }
 </script>
-
-<style scoped>
-ul {
-    list-style: none;
-}
-
-li {
-    display: flex;
-    flex-flow: column;
-    border-radius: 0.5rem;
-    overflow: hidden;
-    box-shadow: var(--shadow);
-}
-
-li>img:first-child {
-    z-index: -1;
-    width: 100%;
-    height: auto;
-    margin-bottom: 0;
-    aspect-ratio: 2/1;
-    object-fit: cover;
-}
-
-li>article {
-    margin: 0;
-    padding: 1rem;
-    background-color: var(--background);
-    color: var(--text);
-    z-index: 1;
-    width: 100%;
-}
-
-li p {
-    margin: 0 !important;
-}
-</style>
