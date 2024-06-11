8957605d1706dc4291622207b5428f73520ded08
Quinn Gale <q.gale@live.com>
2024-06-10 21:36:18 -0600

    New icons

--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -1,13 +1,13 @@
 import { library, config } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-import { faBriefcase, faCat, faDatabase, faEnvelope, faFileCertificate, faGraduationCap, faHeadset, faTag } from '@fortawesome/pro-solid-svg-icons';
+import { faBriefcase, faCat, faDatabase, faEnvelope, faFileCertificate, faGraduationCap, faHeadset, faTag, faChevronRight } from '@fortawesome/pro-solid-svg-icons';
 import { faAngleDown, faArrowUpRightFromSquare } from '@fortawesome/pro-regular-svg-icons'
 import { faApple, faCss3, faGit, faGithub, faHtml5, faLinux, faJs, faMicrosoft, faPhp, faPython, faSass, faVuejs, faWindows } from '@fortawesome/free-brands-svg-icons';
 
 config.autoAddCss = false;
 // Solid icons
-library.add(faBriefcase, faCat, faDatabase, faEnvelope, faFileCertificate, faGraduationCap, faHeadset);
+library.add(faBriefcase, faCat, faDatabase, faEnvelope, faFileCertificate, faGraduationCap, faHeadset, faChevronRight);
 
 // Regular icons
 library.add(faAngleDown, faArrowUpRightFromSquare);
