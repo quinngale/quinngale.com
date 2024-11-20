fb2fade24606cfc12bff901f770ec465cb8e630c
Quinn Gale <q.gale@live.com>
2024-11-19 20:13:21 -0700

    New icon

--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 import { faBriefcase, faCat, faDatabase, faEnvelope, faFileCertificate, faGraduationCap, faHeadset, faTag, faChevronRight } from '@fortawesome/pro-solid-svg-icons';
 import { faAngleDown, faArrowUpRightFromSquare } from '@fortawesome/pro-regular-svg-icons'
-import { faApple, faCss3, faGit, faGithub, faHtml5, faLinux, faJs, faMicrosoft, faPhp, faPython, faSass, faVuejs, faWindows } from '@fortawesome/free-brands-svg-icons';
+import { faApple, faCss3, faGit, faGithub, faHtml5, faLinux, faJs, faMicrosoft, faPhp, faPython, faSass, faVuejs, faWindows, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 config.autoAddCss = false;
 // Solid icons
@@ -13,8 +13,8 @@ library.add(faBriefcase, faCat, faDatabase, faEnvelope, faFileCertificate, faGra
 library.add(faAngleDown, faArrowUpRightFromSquare);
 
 // Brand icons
-library.add(faApple, faCss3, faGit, faGithub, faHtml5, faLinux, faJs, faMicrosoft, faPhp, faPython, faSass, faTag, faVuejs, faWindows);
+library.add(faApple, faCss3, faGit, faGithub, faHtml5, faLinux, faJs, faMicrosoft, faPhp, faPython, faSass, faTag, faVuejs, faWindows, faLinkedin);
 
 export default defineNuxtPlugin((nuxtApp) => {
-    nuxtApp.vueApp.component('FontAwesomeIcon', FontAwesomeIcon, {});
+	nuxtApp.vueApp.component('FontAwesomeIcon', FontAwesomeIcon, {});
 })
